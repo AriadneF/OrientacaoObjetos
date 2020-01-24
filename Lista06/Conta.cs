87d73334819c755d28289d6d8c4964f753772a75
Ariadne Fabian <fabianariadne@gmail.com>
2020-01-24 13:18:39 -0300

    seção 5 finalizada

---
+++ Lista06/Conta.cs
@@ -0,0 +1,35 @@
+﻿namespace Lista06
+{
+    public class Conta
+    {
+        public Conta(int numeroConta, string titular)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+        }
+
+        public Conta(int numeroConta, string titular, double saldo) : this(numeroConta, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo = Saldo + quantia;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + NumeroConta + ", " + "Titular: " + Titular + ", " + "Saldo: R$ " + Saldo.ToString("F2");
+        }
+
+        public int NumeroConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+    }
+}
--- Lista06/Program.cs
+++ Lista06/Program.cs
@@ -6,7 +6,43 @@ namespace Lista06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Conta conta;
+            double quantia; 
+
+            Console.Write("Nome do titular: ");
+            string titular = Console.ReadLine();
+            Console.Write("Numero da conta: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Será feito deposito(sim/nao)? ");
+            string aceito = Console.ReadLine();
+            if(aceito == "sim")
+            {
+                Console.Write("Qual será o valor? ");
+                double valor = Convert.ToDouble(Console.ReadLine());
+                conta = new Conta(numero, titular, valor);
+            }
+            else
+            {
+                conta = new Conta(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Qual valor será depositado? ");
+            quantia = Convert.ToDouble(Console.ReadLine());
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Qual valor será depositado? ");
+            quantia = Convert.ToDouble(Console.ReadLine());
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
         }
     }
 }
