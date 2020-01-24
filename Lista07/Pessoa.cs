cb48a4485214d8829924c4436347a2f017e16e0c
Ariadne Fabian <fabianariadne@gmail.com>
2020-01-24 13:55:38 -0300

    Lista07 pronta

---
+++ Lista07/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista07
+{
+    public class Pessoa
+    {
+        public Pessoa(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email; 
+        }
+
+        public string Nome;
+        public string Email;
+    }
+}
---
+++ Lista07/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lista07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa[] estudante = new Pessoa[10];
+
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            int aluguel = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= aluguel; i++)
+            {
+                Console.WriteLine("Cadastro de aluguel!");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = Convert.ToInt32(Console.ReadLine());
+                estudante[quarto] = new Pessoa(nome, email);
+            }
+
+            Console.WriteLine("Quartso alugados: ");
+            for(int i = 1; i <= aluguel; i++)
+            {
+                if(estudante[i] != null)
+                {
+                    Console.WriteLine(i + ": " + estudante[i]);
+                }
+            }
+        }
+    }
+}
