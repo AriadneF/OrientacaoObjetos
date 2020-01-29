6858e0455fc88564b323d8269c461268c1844297
Ariadne Fabian <fabianariadne@gmail.com>
2020-01-29 11:44:01 -0300

    Exercicios Propostos Membros Estaticos

---
+++ ExerciciosPropostosMembrosEstaticos/Conversao.cs
@@ -0,0 +1,12 @@
+﻿namespace ExerciciosPropostosMembrosEstaticos
+{
+    public class Conversao
+    {
+        public static double IOF = 6.0;
+        public static double Converte(double cotacao, double compra)
+        {
+            double total = compra * cotacao;
+            return total + total * IOF / 100.0;
+        }
+    }
+}
---
+++ ExerciciosPropostosMembrosEstaticos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExerciciosPropostosMembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cotação do dólar no momento: ");
+            double cotacao = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Dinheiro a ser comprado: ");
+            double compra = Convert.ToInt32(Console.ReadLine());
+
+            double resultado = Conversao.Converte(cotacao, compra);
+            Console.WriteLine("Valor a ser pago: " + resultado.ToString("F2"));
+        }
+    }
+}
